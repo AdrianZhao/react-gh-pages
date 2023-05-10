0326f49e6bfe3f56479fa9f78001669873a71268
Adrian Zhao <achnnal@gmail.com>
2023-05-10 10:24:45 -0500

    fix h2

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ function Banner(props) {
     <section className='banner'>
       <div className='filter'>
         <div className='wrapper container'>
-          <h2>Welcome to Fantanstic Cats! Let\'s enjoy plenty of cats photos and videos from our gorgeous cats!</h2>
+          <h2>Welcome to Fantanstic Cats! Let's enjoy plenty of cats photos and videos from our gorgeous cats!</h2>
           <div className='buttonWrapper'>
             <Button isMoreCat={!props} />
             <Button isMoreCat={props} />
